28092bba701a75960539bde2412c1d427aa508b9
quesia-araujo <67472987+quesia-araujo@users.noreply.github.com>
2024-01-05 16:43:39 -0400

    study js

---
+++ estudos/javascript/aula-06/index.js
@@ -0,0 +1,14 @@
+// Camel case (Padrão do JS)
+
+// Tipagem dinâmica: infere os tipos de dados
+// Possui uma tipagem fraca
+
+var nomeDaProfessora = "Quesia Araújo"; //string (texto)
+var idade = 28; //number
+var altura = 1.65 //number
+var estudando = true //boolean
+
+console.log(nomeDaProfessora, typeof nomeDaProfessora)
+console.log(idade, typeof idade)
+console.log(altura, typeof altura)
+console.log(estudando, typeof estudando)
